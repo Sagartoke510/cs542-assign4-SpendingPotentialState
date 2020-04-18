a5c0c2d77aa15edaf648b58e0739fc5943bef06f
sagar <sagar@192.168.0.5>
2020-04-18 15:01:11 -0400

    commandline exception file updated

---
+++ spendingPotentialState/src/spendingPotentialState/cmdLineExceptions/CmdLineInputException.java
@@ -0,0 +1,13 @@
+package spendingPotentialState.cmdLineExceptions;
+/**
+* The class {@code CmdLineInputException} is a form of exception
+*/
+public class CmdLineInputException extends Exception{
+	/**
+	* Constructs a new exception with specified detail message
+	* @param message the detail message
+	*/
+	public CmdLineInputException(String message) {
+		super(message);
+	}
+}
